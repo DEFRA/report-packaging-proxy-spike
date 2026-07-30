2c2fd5aaba4d87c50667d9574ac8b0f1fcd8272d
Tim Peel <tim.peel@equalexperts.com>
2026-07-30 11:53:59 +0100

    Remove Node frontend template before proxy migration

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import neostandard from 'neostandard'
-
-export default neostandard({
-  env: ['node', 'vitest'],
-  ignores: [...neostandard.resolveIgnoresFromGitignore()],
-  noJsx: true,
-  noStyle: true
-})
